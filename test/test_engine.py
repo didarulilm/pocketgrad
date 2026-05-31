155cdc6991e89e8fd6a46e464abd6fc8819564a6
didarul <didarulislam85@hotmail.com>
2026-05-31 08:35:39 +0600

    refactor: replace sanity checks

--- test/test_engine.py
+++ test/test_engine.py
@@ -2,68 +2,80 @@ import torch
 from pocketgrad.engine import Scalar
 
 
-def test_sanity_check():
-
-    x = Scalar(-4.0)
-    z = 2 * x + 2 + x
-    q = z.relu() + z * x
-    h = (z * z).relu()
-    y = h + q + q * x
-    y.backward()
-    xmg, ymg = x, y
-
-    x = torch.Tensor([-4.0]).double()
-    x.requires_grad = True
-    z = 2 * x + 2 + x
-    q = z.relu() + z * x
-    h = (z * z).relu()
-    y = h + q + q * x
-    y.backward()
-    xpt, ypt = x, y
-
-    # forward pass went well
-    assert ymg.data == ypt.data.item()
-    # backward pass went well
-    assert xmg.grad == xpt.grad.item()
+def assert_close(actual, expected, tol=1e-6):
+    assert abs(actual - expected) < tol
 
 
-def test_more_ops():
+def test_chain_expr():
+    x = Scalar(-3.5)
+    y = Scalar(1.25)
 
-    a = Scalar(-4.0)
-    b = Scalar(2.0)
-    c = a + b
-    d = a * b + b**3
-    c += c + 1
-    c += 1 + c + (-a)
-    d += d * 2 + (b + a).relu()
-    d += 3 * d + (b - a).relu()
-    e = c - d
-    f = e**2
-    g = f / 2.0
-    g += 10.0 / f
-    g.backward()
-    amg, bmg, gmg = a, b, g
+    out = ((x * y) + x.relu()) * (y + 2.0) + (x - y) ** 2
+    out.backward()
 
-    a = torch.Tensor([-4.0]).double()
-    b = torch.Tensor([2.0]).double()
-    a.requires_grad = True
-    b.requires_grad = True
-    c = a + b
-    d = a * b + b**3
-    c = c + c + 1
-    c = c + 1 + c + (-a)
-    d = d + d * 2 + (b + a).relu()
-    d = d + 3 * d + (b - a).relu()
-    e = c - d
-    f = e**2
-    g = f / 2.0
-    g = g + 10.0 / f
-    g.backward()
-    apt, bpt, gpt = a, b, g
+    x_pg, y_pg, out_pg = x, y, out
 
-    tol = 1e-6
-    # forward pass went well
-    assert abs(gmg.data - gpt.data.item()) < tol
-    # backward pass went well
-    assert abs(amg.grad - apt.grad.item()) < tol
-    assert abs(bmg.grad - bpt.grad.item()) < tol
+    x = torch.tensor([-3.5], dtype=torch.double, requires_grad=True)
+    y = torch.tensor([1.25], dtype=torch.double, requires_grad=True)
+
+    out = ((x * y) + x.relu()) * (y + 2.0) + (x - y) ** 2
+    out.backward()
+
+    x_th, y_th, out_th = x, y, out
+
+    assert_close(out_pg.data, out_th.item())
+    assert_close(x_pg.grad, x_th.grad.item())
+    assert_close(y_pg.grad, y_th.grad.item())
+
+
+def test_reused_subgraph():
+    a = Scalar(2.0)
+    b = Scalar(-1.5)
+
+    shared = a * b + a + 0.5
+    out = shared * shared + shared.relu() + b / a
+    out.backward()
+
+    a_pg, b_pg, out_pg = a, b, out
+
+    a = torch.tensor([2.0], dtype=torch.double, requires_grad=True)
+    b = torch.tensor([-1.5], dtype=torch.double, requires_grad=True)
+
+    shared = a * b + a + 0.5
+    out = shared * shared + shared.relu() + b / a
+    out.backward()
+
+    a_th, b_th, out_th = a, b, out
+
+    assert_close(out_pg.data, out_th.item())
+    assert_close(a_pg.grad, a_th.grad.item())
+    assert_close(b_pg.grad, b_th.grad.item())
+
+
+def test_mixed_ops():
+    p = Scalar(-0.75)
+    q = Scalar(3.0)
+    r = Scalar(0.5)
+
+    m = (p + q) * r
+    n = (m.relu() + q / 2.0) * (p - r)
+    out = n + p**2 - (q**2) / 3.0 + 4.0
+    out.backward()
+
+    p_pg, q_pg, r_pg, out_pg = p, q, r, out
+
+    p = torch.tensor([-0.75], dtype=torch.double, requires_grad=True)
+    q = torch.tensor([3.0], dtype=torch.double, requires_grad=True)
+    r = torch.tensor([0.5], dtype=torch.double, requires_grad=True)
+
+    m = (p + q) * r
+    n = (m.relu() + q / 2.0) * (p - r)
+    out = n + p**2 - (q**2) / 3.0 + 4.0
+    out.backward()
+
+    p_th, q_th, r_th, out_th = p, q, r, out
+
+    assert_close(out_pg.data, out_th.item())
+    assert_close(p_pg.grad, p_th.grad.item())
+    assert_close(q_pg.grad, q_th.grad.item())
+    assert_close(r_pg.grad, r_th.grad.item())
